d6716c6d9cef948e340cf577bcfc866f1ef64c76
RSTM12 <140751554+RSTM12@users.noreply.github.com>
2026-07-18 01:02:40 +0700

    Create config.ts

---
+++ game/config.ts
@@ -0,0 +1,28 @@
+import Phaser from "phaser";
+
+export const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+
+  width: 1280,
+  height: 720,
+
+  backgroundColor: "#0b1116",
+
+  physics: {
+    default: "arcade",
+
+    arcade: {
+      gravity: {
+        x: 0,
+        y: 0,
+      },
+
+      debug: false,
+    },
+  },
+
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
